2fa6ce44f9d6add14bc3f1bd43faa002bc0a51ec
Hackim18 <khakim668@gmail.com>
2024-06-02 16:00:17 +0700

    feat: add initial setup

---
+++ back-end-spring-boot-java/src/main/java/com/simple_order_system/back_end_spring_boot_java/BackEndSpringBootJavaApplication.java
@@ -0,0 +1,13 @@
+package com.simple_order_system.back_end_spring_boot_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndSpringBootJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndSpringBootJavaApplication.class, args);
+	}
+
+}
---
+++ back-end-spring-boot-java/src/test/java/com/simple_order_system/back_end_spring_boot_java/BackEndSpringBootJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simple_order_system.back_end_spring_boot_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndSpringBootJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
